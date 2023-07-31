88e2ed066677d3cf243294ecc7789f8cd5ae97e3
SpiralMisT <thomas.ton71@gmail.com>
2023-07-31 14:49:27 -0400

    remove

--- EraTrader/src/trader.py
+++ EraTrader/src/trader.py
@@ -8,7 +8,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-private_key = os.getenv('PRIVATE_KEY')
+wallets = [key for key in os.environ.keys() if key.startswith("PRIVATE_KEY")]  
+
+# private_key = os.getenv('PRIVATE_KEY')
 
 sdk = ZkSyncBuilder.build("https://testnet.era.zksync.dev")
 
@@ -19,15 +21,17 @@ ZKSYNC_PROVIDER = "https://zksync2-testnet.zksync.dev"
 # ZKSYNC_PROVIDER = "https://rpc.ankr.com/eth_goerli"
 
 # Checks Balance
-def check_balance():
-    env = EnvPrivateKey("PRIVATE_KEY")
+def check_balance(wallet):
+    env = EnvPrivateKey(wallet)
     account: LocalAccount = Account.from_key(env.key)
-    print(account.address)
-    print(account)
+    print('Address: ' + account.address)
     zksync_web3 = ZkSyncBuilder.build(ZKSYNC_PROVIDER)
     zk_balance = zksync_web3.zksync.get_balance(account.address, EthBlockParams.LATEST.value)
     print(f"Balance: {zk_balance}")
 
 
 if __name__ == "__main__":
-    check_balance()
+    for wallet in wallets:
+        print(f"Checking balance for {wallet}:")
+        check_balance(wallet)
+        print()
